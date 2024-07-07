75f54d5c43ff77e2418fb5369d5c24c1719fccfa
momenw2 <momen2550@icloud.com>
2024-07-07 17:10:35 +0700

    Added order route to app.js

--- app.js
+++ app.js
@@ -27,9 +27,13 @@ app.get("/item", (req, res) => res.render("itemPage"));
 app.get("/checkout", (req, res) => res.render("checkoutPage"));
 app.get("/confirmation", (req, res) => res.render("confirmationPage"));
 
-// Product  Router
+// Product Router
 const productRoutes = require("./Routes/product.router");
 app.use("/productBCK", productRoutes);
+
+// Order Router
+const orderRoutes = require("./Routes/order.router");
+app.use("/orderBCK", orderRoutes);
 
 //Error Handler
 app.use((req, res, next) => {
